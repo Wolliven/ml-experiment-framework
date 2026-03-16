ec4bbff24155dfb3e545bd4b2aa305ff40ece660
Jerico San Nicolas <jericosnicolas@gmail.com>
2026-03-16 14:28:34 +0900

    feat: add CLI logic for run_experiment.py and placeholder code

---
+++ ml_engine/training.py
@@ -0,0 +1,7 @@
+"""
+This module contains the training logic for the machine learning model. It calls functions to load data, preprocess it, and train the model using the specified parameters. 
+"""
+
+def train_model(experiment_path : str = None) -> dict:
+    experiment_path = experiment_path
+    return experiment_path
---
+++ scripts/run_experiment.py
@@ -0,0 +1,30 @@
+"""
+Entrypoint for training the experiment.
+This script loads a dataset from a CSV file and a experiment yaml file, preprocesses the data,
+trains a model, evaluates its performance, saves
+the trained model to disk and saves the experiment results to a JSON file.
+"""
+import sys
+import argparse
+
+def main() -> None:
+    from ml_engine.training import train_model
+    parser = argparse.ArgumentParser(
+        description="Run a machine learning experiment based on the provided YAML configuration file.",
+        epilog="Example: python scripts/run_experiment.py configs/experiment_001.yaml"
+    )
+    parser.add_argument("experiment", type=str, help="Path to the experiment YAML file.")
+    args = parser.parse_args()
+
+    result = train_model(experiment_path=args.experiment)
+    print(f"Experiment completed. Result: {result}")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nTraining interrupted by user. Exiting.")
+        sys.exit(130)
+    except (FileNotFoundError, ValueError) as e:
+        print(e)
+        sys.exit(1)
